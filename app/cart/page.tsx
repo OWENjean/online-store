561427e8a33d6066751193c9f11aa5c9a0f8702b
jean claud <125636329+OWENjean@users.noreply.github.com>
2026-03-25 15:11:30 +0300

    Create shopping cart page

---
+++ app/cart/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CartPage = () => {
+    return (
+        <div>
+            <h1>Your Shopping Cart</h1>
+            <p>This is the shopping cart page.</p>
+            {/* Add your cart items and functionalities here */}
+        </div>
+    );
+};
+
+export default CartPage;
